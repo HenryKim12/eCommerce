e7fc6f202b3bf1a42975a78bb762ae96f54be606
henrykim12 <henrykim966@gmail.com>
2024-03-14 21:57:09 -0700

    test view

--- src/views.py
+++ src/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def home(request):
+    #products = Product.objects.create(name="test")
     return HttpResponse("hello world, this is home")
